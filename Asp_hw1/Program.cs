48990aa3fddd43a7284e91f2df7aa7d188f3809a
Kostya <k.konstantin2212@gmail.com>
2023-09-11 19:12:40 +0300

    Добавьте файлы проекта.

---
+++ Asp_hw1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace asp_net_20_07
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = WebApplication.CreateBuilder(args).Build();
+
+            app.MapGet("/customs_duty", (int price) =>
+            {
+                if (price > 200)
+                {
+                    return "Стоимость привышает 200€, пошлина будет составлять: " + (Convert.ToDouble(price) * 0.15) + "€";
+                }
+                return "Стоимость не превышает 200€!";
+            });
+
+            app.MapGet("/", () => DateTime.Now.ToString(" HH:mm:ss\ndddd, dd MMM", new CultureInfo(CultureInfo.CurrentCulture.Name.ToString())));
+
+            app.Run();
+        }
+    }
+}
